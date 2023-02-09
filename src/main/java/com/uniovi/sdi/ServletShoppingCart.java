e718707ce1e96d03d5cd362b4bc199474854600d
Patri <uo282210@uniovi.es>
2023-02-09 17:36:26 +0100

    SDI-2223-304-1.2-Manejo de sesiones.”

---
+++ src/main/java/com/uniovi/sdi/ServletShoppingCart.java
@@ -0,0 +1,64 @@
+package com.uniovi.sdi;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.HashMap;
+
+@WebServlet(name = "ServletShoppingCart", value = "/AddToShoppingCart")
+public class ServletShoppingCart extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        // obtener la sesion del requesr
+        HttpSession session = request.getSession();
+        // de la sesion obtener el carrito y hacer un cast a hashmap
+        HashMap<String, Integer> cart =
+                (HashMap<String, Integer>) request.getSession().getAttribute("cart");
+
+        // No hay carrito, creamos uno y lo insertamos en sesión
+        if (cart == null) {
+            // crear el carrito
+            cart = new HashMap<String, Integer>();
+            // establecer el valor del nuevo carrito al atributo cart de la sesion
+            request.getSession().setAttribute("cart", cart);
+        }
+        // sacar el nombre del producto de la request
+        String product = request.getParameter("product");
+        // si el producto no es nulo, añadirlo al carrito
+        if (product != null) {
+            addToShoppingCart(cart, product);
+        }
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<HTML>");
+        out.println("<HEAD><TITLE>Tienda SDI: Cesta de la compra</TITLE></HEAD>");
+        out.println("<BODY>");
+        out.println(shoppingCartToHtml(cart) + "<br>");
+        out.println("<a href=\"shop.html\">Volver</a></BODY></HTML>");
+    }
+
+    private void addToShoppingCart(HashMap<String, Integer> cart, String productKey){
+        if (cart.get(productKey) == null)
+            cart.put(productKey, Integer.valueOf(1));
+        else{
+            int productCount = (Integer) cart.get(productKey).intValue();
+            cart.put(productKey, Integer.valueOf(productCount + 1));
+        }
+    }
+
+    private String shoppingCartToHtml(HashMap<String,Integer> cart){
+        String shoppingCartToHtml = "";
+
+        for (String key : cart.keySet()){
+            shoppingCartToHtml += "<p>[" + key + "]," + cart.get(key) + " unidades</p>";
+        }
+        return shoppingCartToHtml;
+    }
+
+
+}
